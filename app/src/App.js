e828fbbe8cb97032ee7df373dfec54eaaa3e0fe2
gilbertVirgo <gilbertjvirgo@gmail.com>
2019-09-13 17:45:59 +0100

    Added download button

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,11 @@
 import React, {useState} from 'react';
 
 import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { FaDownload } from 'react-icons/fa';
 
 import Toolbar from "./components/Toolbar";
+
 
 const App = () => {
   const [image, setImage] = useState("");
@@ -16,7 +19,13 @@ const App = () => {
       <p>Some pop art</p>
       <hr/>
       <Toolbar onChange={handleChange}/>
-      {image && <img style={{maxWidth: "500px", width: "100%"}} src={image}/>}
+      {image && (<>
+        <img style={{maxWidth: "500px", width: "100%"}} src={image}/>
+        
+        <a download="popart.png" href={image} style={{display: "block", margin: "15px 0 0"}}>
+          <Button variant="dark">Download <FaDownload/></Button>
+        </a>
+      </>)}
     </Container>
   );
 }
