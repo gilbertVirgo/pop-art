2903a52f6dc70928ff5d8568a9fac2f249a2c69d
gilbertVirgo <gilbertjvirgo@gmail.com>
2019-09-14 00:08:33 +0100

    Added share buttons

--- app/src/App.js
+++ app/src/App.js
@@ -2,10 +2,15 @@ import React, {useState} from 'react';
 
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 import { FaDownload } from 'react-icons/fa';
 
 import Toolbar from "./components/Toolbar";
 
+import {
+  FacebookShareButton, FacebookIcon,
+  TwitterShareButton, TwitterIcon
+} from "react-share";
 
 const App = () => {
   const [image, setImage] = useState("");
@@ -23,9 +28,17 @@ const App = () => {
       {image && (<>
         <img style={{maxWidth: "500px", width: "100%"}} src={image}/>
         
-        <a download={`popart-${Date.now()}.png`} href={image} style={{display: "block", margin: "15px 0 0"}}>
-          <Button variant="dark">Download <FaDownload/></Button>
-        </a>
+        <ButtonGroup style={{display: "block", margin: "15px 0"}}>
+          <a download={`popart-${Date.now()}.png`} href={image} style={{display: "inline-block", marginRight: "15px"}}>
+            <Button variant="dark">Download <FaDownload/></Button>
+          </a>
+          <FacebookShareButton url={window.location.href} style={{display: "inline-block", marginRight: "15px"}} quote="I just created a thing" hashtag="#filter">
+            <FacebookIcon size="30px" round />
+          </FacebookShareButton>
+          <TwitterShareButton url={window.location.href} style={{display: "inline-block", marginRight: "15px"}} title="I just created a thing">
+            <TwitterIcon size="30px" round />
+          </TwitterShareButton>
+        </ButtonGroup>
       </>)}
     </Container>
   );
